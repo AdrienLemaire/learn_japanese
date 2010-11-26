9b37cb1c44ca20ceab0e04c5a4d5c9ee0fa4ba62
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-11-26 14:56:17 +0000

    use of bmp image to get something working

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+###########
+##########
+#########-*- coding:utf-8 -*-
+
 import pygame
 from pygame.locals import *
 
@@ -8,10 +12,17 @@ pygame.init()
 # New window
 window = pygame.display.set_mode((640, 480), RESIZABLE)
 
-background = pygame.image.load("background.jpg").convert()
-window.blit(window, (0, 0))
+background = pygame.image.load("background.bmp").convert()
+window.blit(background, (0, 0))
 pygame.display.flip()
 
 next = 1
 while next:
-    next = int(input())
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            next = 0
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                print("Space")
+            if event.key == K_RETURN:
+                print("Enter")
